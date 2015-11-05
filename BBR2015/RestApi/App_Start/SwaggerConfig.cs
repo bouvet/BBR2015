54c5cb954c37b1e4dc83303b03dd3d42d50ebcbf
Sølve Heggem <solve.heggem@bouvet.no>
2015-11-05 19:25:59 +0100

    Initial swagger config - still much to hide

--- BBR2015/RestApi/App_Start/SwaggerConfig.cs
+++ BBR2015/RestApi/App_Start/SwaggerConfig.cs
@@ -96,7 +96,7 @@ namespace RestApi
                         // those comments into the generated docs and UI. You can enable this by providing the path to one or
                         // more Xml comment files.
                         //
-                        //c.IncludeXmlComments(GetXmlCommentsPath());
+                        c.IncludeXmlComments(GetXmlCommentsPath());
 
                         // Swashbuckle makes a best attempt at generating Swagger compliant JSON schemas for the various types
                         // exposed in your API. However, there may be occasions when more control of the output is needed.
@@ -219,6 +219,12 @@ namespace RestApi
                         //
                         //c.EnableOAuth2Support("test-client-id", "test-realm", "Swagger UI");
                     });
+        }
+
+        private static string GetXmlCommentsPath()
+        {
+            return string.Format(@"{0}\bin\RestApi.XML",
+                System.AppDomain.CurrentDomain.BaseDirectory);
         }
     }
 }
