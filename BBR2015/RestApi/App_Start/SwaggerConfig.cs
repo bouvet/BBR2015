a5f2ec9a590ad07ba919d3e381befdbab37f74c7
Rune Rystad <rune.rystad@gmail.com>
2016-08-14 21:57:34 +0200

    Moved testclient and scoreboard into webapi project for easier deployment

--- BBR2015/RestApi/App_Start/SwaggerConfig.cs
+++ BBR2015/RestApi/App_Start/SwaggerConfig.cs
@@ -32,7 +32,7 @@ namespace RestApi
                         // hold additional metadata for an API. Version and title are required but you can also provide
                         // additional fields by chaining methods off SingleApiVersion.
                         //
-                        c.SingleApiVersion("v1", "Bouvet Battle Royal 2015");
+                        c.SingleApiVersion("v1", "Bouvet Battle Royale");
 
                         // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
                         // In this case, you must provide a lambda that tells Swashbuckle which actions should be
